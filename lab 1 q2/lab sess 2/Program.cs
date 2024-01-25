6c24359f21e1ee7da850657df38b90d1d581704c
senuri perera <90140640+senuriperera@users.noreply.github.com>
2024-01-25 11:53:19 +0530

    lab 1/q2

---
+++ lab 1 q2/lab sess 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_sess_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Enter number "+ (i+1) +":");
+
+                    int num = Convert.ToInt16(Console.ReadLine());
+
+                    if (num % 2 == 0)
+                    {
+                        Console.WriteLine("Even number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Odd Number");
+                    }
+
+                 
+                }
+            }
+        }
+    }
+}
+
