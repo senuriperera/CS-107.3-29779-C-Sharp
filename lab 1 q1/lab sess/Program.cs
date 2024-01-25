ecaf96a400c44d35ab46eb1addad463bec1e69df
senuri perera <90140640+senuriperera@users.noreply.github.com>
2024-01-25 11:52:17 +0530

    lab 1/q1

---
+++ lab 1 q1/lab sess/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_sess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input length");
+            int length=Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Input width");
+            int width=Convert.ToInt16(Console.ReadLine());
+
+            int CalculateArea= Area(length, width);
+
+            Console.WriteLine($"Area is {CalculateArea}");
+            Console.ReadLine();
+        }
+        static int Area(int length,int width)
+        {
+            int area = length * width;
+            return area;
+        }
+    }
+}
