0f8618de293a94a8ef5d2ccc69a1baf3a2e8500f
senuri perera <90140640+senuriperera@users.noreply.github.com>
2024-02-05 13:31:07 +0530

    lab1/q5

---
+++ lab 1 q5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of the multiplication table needed");
+            int num =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter upto what times(number) you need");
+            int length= int.Parse(Console.ReadLine());
+
+           
+            for(int i = 1; i <= length; i++) 
+            { 
+              int ans = i * num;
+                Console.WriteLine(i + " x " + num + " = " + ans);   
+            }
+            Console.ReadLine();
+        }
+    }
+}
