3057c375b25ba78707f54cb21e7e1509f467c628
senuri perera <90140640+senuriperera@users.noreply.github.com>
2024-02-05 13:32:26 +0530

    lab1/q6

---
+++ lab 1 q6/lab 1 q6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the student name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter the marks of the students");
+            int mark = int.Parse(Console.ReadLine());
+
+            
+                if (mark >= 75 && mark <= 100)
+                {
+                    Console.WriteLine(name + ": Grade A");
+                  
+                }
+                else if (mark >= 60 && mark <= 74)
+                {
+                    Console.WriteLine(name + ": Grade B");
+                   
+                }
+                else if (mark >= 50 && mark <= 59)
+                {
+                    Console.WriteLine(name + ": Grade C");
+                  
+                }
+                else if (mark >= 40 && mark <= 49)
+                {
+                    Console.WriteLine(name + ": Grade D");
+                }
+                else if (mark < 40)
+                {
+                    Console.WriteLine(name + ": Failed");
+                }
+                else if (mark < 0 && mark > 100)
+                {
+                    Console.WriteLine(name + ": Error!");
+                }
+            Console.ReadLine();
+
+        }
+    }
+}
