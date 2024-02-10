30d4948b68456859e41b04d77275c3df5b24d325
senuri perera <90140640+senuriperera@users.noreply.github.com>
2024-02-10 13:02:37 +0530

    lab 2/q1

---
+++ lab 2 q1/lab 2 q1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "J.K.Rowling";
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.ReadLine();
+        }
+    }
+    class Book
+    {
+        public string title;
+        public string author;
+    }
+}
