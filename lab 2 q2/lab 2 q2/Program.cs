526f192d47a60c158bded9914ca41604ac39e36e
senuri perera <90140640+senuriperera@users.noreply.github.com>
2024-02-10 13:03:20 +0530

    lab 2 /q2

---
+++ lab 2 q2/lab 2 q2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab_2_q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the account number: ");
+            int accountNumber = Convert.ToInt32(Console.ReadLine());
+
+            BankAccount account = new BankAccount(accountNumber);
+
+            Console.WriteLine("Enter the amount to be deposited: ");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+
+            account.Deposit(depositAmount);
+
+            Console.WriteLine("Updated Balance: " + account.GetBalance());
+            Console.ReadLine();
+        }
+    }
+
+    class BankAccount
+    {
+        private int accountNumber;
+        private double balance;
+
+        public BankAccount(int accountNumber)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = 0; 
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount; 
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
