374348db6ba91369c387679352b3e475f0b41c91
senuri perera <pereralsd@students.nsbm.ac.lk>
2024-02-13 16:39:36 +0530

    lab 2/q4

---
+++ lab 2 q4/lab 2 q4/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_q4
+{
+    internal class Product
+    {
+        public string ProductName;
+        public double Price;
+
+        public Product(string proName, double proPrice)
+        {
+            ProductName = proName;
+            Price = proPrice;
+        }
+    }
+}
---
+++ lab 2 q4/lab 2 q4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Iphone",79999.99);
+            Product product2 = new Product("Smart TV",125000);
+
+            Console.WriteLine("Product Name:" + product1.ProductName);
+            Console.WriteLine("Price: Rs." + product1.Price);
+            Console.WriteLine("Product Name:" + product2.ProductName); 
+            Console.WriteLine("Price: Rs." + product2.Price);
+            Console.ReadLine();
+        }
+    }
+}
