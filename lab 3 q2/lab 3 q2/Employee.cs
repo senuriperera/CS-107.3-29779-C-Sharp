5900e5f0fbfecec0113254ba19edc3ae7c0f8979
senuri perera <pereralsd@students.nsbm.ac.lk>
2024-02-18 11:49:31 +0530

    lab 3/q2

---
+++ lab 3 q2/lab 3 q2/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3_q2
+{
+    internal class Employee
+    {
+        private int employeeID;
+        private string FullName;
+        private double Salary;
+
+        public int EmployeeID
+        {
+            get
+            {
+                return employeeID;
+            }
+        }
+        public string fullName
+        {
+            get { return FullName; }
+            set { FullName= value;}
+        }
+        public double salary
+        {
+            get { return Salary; }
+            set { Salary= value;}
+        }
+
+        public Employee(int employeeID, string FullName,double Salary)
+        {
+            this.employeeID = employeeID;
+            this.FullName = FullName;
+            this.Salary = Salary;
+        }
+        public void DisplayEmployeeInfo()
+        {
+
+            Console.WriteLine($"Employee ID: {EmployeeID}");
+            Console.WriteLine($"Full Name: {FullName}");
+            Console.WriteLine($"Salary: Rs.{Salary}");
+        }
+    }
+}
---
+++ lab 3 q2/lab 3 q2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_3_q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(101,"John Doe",50000);
+
+            employee1.DisplayEmployeeInfo();
+
+            Console.WriteLine("Updated Employee Information:");
+            //updating name
+            employee1.fullName = "Krystal Smith";
+            employee1.salary = 60000;
+            employee1.DisplayEmployeeInfo();
+            Console.ReadLine();
+        }
+    }
+}
